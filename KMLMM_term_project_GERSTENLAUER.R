929d10fc5801bfd1e4fb86f2d87bb4aac0f1f04b
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2016-11-26 14:03:54 +0100

    update of comments

--- KMLMM_term_project_GERSTENLAUER.R
+++ KMLMM_term_project_GERSTENLAUER.R
@@ -1,8 +1,8 @@
 ###############################################################################################
 # Kernel-Based Learning & Multivariate Modeling DMKM Master - MIRI Master
-# Lecturer: Lluıs A. Belanche belanche@cs.upc.edu
+# Lecturer: Lluıs A. Belanche, belanche@cs.upc.edu
 # Term project
-# October / November 2016  
+# October-Dezember 2016  
 # "Compare the Relevance Vector machine and the SVM for regression in terms of predictive accuracy 
 # and sparsity."
 #
@@ -16,11 +16,15 @@
 #   Based on the optimum parameters (both kernel and RVM parameters, criteria: cv error) 
 #   we record generalization error, sparsity, and total computation time (including tuning) for the selected model.
 # Seventh, we analyse the results of step 5 and 6 comparing SVM and RVM across the following dimensions:
-#   1. decreasing signal to noise ratio,
-#   2. increasing number of predictors P,
-#   3. increasing number of observatios N.
+#   dim 1: signal-to-noise ratio (between 0: no signal only noise and 1: only signal no noise),
+#   dim 2: number of observations N,
+#   dim 3: number of parameters D,
+#   dim 4: polynomial degree of the inputs.
+# Eight, we compare the two models asking the following questions:
+#   Question 1: How often do the models choose the appropriate ploynomial degree of the kernel?
+#   Question 2: How does the predictive accuracy of both models depend on N, D, and signal-to-noise ratio?
 #
-# Date: 27.10.2016
+# Date: 26.11.2016
 # Jakob Gerstenlauer
 # jakob.gerstenlauer@gjakob.de
 ###############################################################################################
