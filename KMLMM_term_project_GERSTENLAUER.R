c0826ea989698f5f324a54534e2cf1380cc76de9
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2016-12-03 18:21:25 +0100

    Write data set with results to file and remove data analysis of results from script.

--- KMLMM_term_project_GERSTENLAUER.R
+++ KMLMM_term_project_GERSTENLAUER.R
@@ -287,18 +287,5 @@ d.results<-data.frame(
   sparsity,
   sd.sparsity)
 
-#Was the correct polynomial degree chosen?
-plot(polynomial.degree.grid,polynomial_degree_setting)
-
-#How do the chosen C and Epsilon relate to signal-to-noise ratio,
-#N and D?
-
-m1_c.lm<-lm(c_setting ~ signal.to.noise.ratio.grid + num.vars.grid + num.observations.grid)
-m1_epsilon.lm<-lm(epsilon_setting ~ signal.to.noise.ratio.grid + num.vars.grid + num.observations.grid)
-
-#How does the performance of the SVM depend on
-#signal-to-noise ratio,
-#N and D?
-
-m1_cv_mean.lm<-lm(cv.mean ~ signal.to.noise.ratio.grid + num.vars.grid + num.observations.grid)
-m1_sparsity.lm<-lm(sparsity ~ signal.to.noise.ratio.grid + num.vars.grid + num.observations.grid)
+setwd(dataDir)
+write.table(d.results, file=fileName, append=FALSE, row.names = FALSE)
