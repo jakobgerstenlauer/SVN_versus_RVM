1fd66fcd2619865df391833bdc6ac9393a022b1a
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2016-12-03 18:46:31 +0100

    add includes and read in data file

--- DataAnalysisResults.R
+++ DataAnalysisResults.R
@@ -1,3 +1,25 @@
+#remove old objects for safety resons
+rm(list=ls(all=TRUE))
+
+#utility function
+glue<-function(...){paste(...,sep="")}
+
+#TODO Adapt to working dir or remove!
+#setwd('..')
+#setwd(glue(getwd(),'/code'))
+#setwd("J:/UPC/2016/02/KMLMM/KernelMethods/practicals/term_project/code")
+
+source("properties.R")
+#define path of standard directories
+source("workingDir.R")
+
+setwd(dataDir)
+#get current date and replace hyphens by underline
+Date<-gsub(pattern="-", replacement="_",Sys.Date())
+#TODO: Check if the date is correct!
+#paste new filename
+fileName<-paste("Results_Simulation_KMLMM_term_project_",Date,".csv",sep="")
+d<-read.table(d.results, file=fileName, append=FALSE, row.names = FALSE)
 
 #Was the correct polynomial degree chosen?
 plot(polynomial.degree.grid,polynomial_degree_setting)
