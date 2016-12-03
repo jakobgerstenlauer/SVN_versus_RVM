491d6d8a94383a21e003599c7c9ae6fe7e7c4679
raizend1 <pacogppl@hotmail.com>
2016-12-03 16:33:10 +0100

    Read table

--- analysis.R
+++ analysis.R
@@ -17,7 +17,7 @@ source("workingDir.R")
 setwd(dataDir)
 
 #import log from file
-data.log.file <- read.table("Log_KMLMM_term_project_2016_12_03.log",header = T)
+data.log.file <- read.table("Log_KMLMM_term_project_2016_11_30.log",header = T)
 ggplot(data.log.file,aes(x=data.log.file$opt.step,y=data.log.file$comput.time,fill=data.log.file$parameter))+geom_bar(position = "dodge",stat="identity")+labs(title="Comparison for each parameter in each step",x="Step",y="Computation Time") 
 
 #get the computation time and length for each parameter
@@ -28,3 +28,5 @@ tapply(data.log.file$comput.time,data.log.file$parameter,length)
 tapply(data.log.file$comput.time,data.log.file$opt.step,sum)
 tapply(data.log.file$comput.time,data.log.file$opt.step,length)
 
+tapply(data.log.file$comput.time,data.log.file$parameter,ave)
+ave(data.log.file$comput.time)
