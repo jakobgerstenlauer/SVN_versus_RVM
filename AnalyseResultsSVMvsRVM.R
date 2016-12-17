1e3783880bcfa6ac841461b18e93366657b0bf54
Francisco Perez <pacogppl@hotmail.com>
2016-12-17 16:28:31 +0100

    Round numbers of the table to show percentaje

--- AnalyseResultsSVMvsRVM.R
+++ AnalyseResultsSVMvsRVM.R
@@ -27,10 +27,24 @@ source("workingDir.R")
 
 #this region is for static loading
 setwd(dataDir)
-d.svm<-read.csv("Results_Simulation_RVM_KMLMM_term_project_2016_12_08.csv",sep=";")
+d.svm<-read.csv("SVM_results_table_KMLMM_term_project_2016_12_17.csv",sep=";")
 str(d.svm)
-d.rvm<-read.csv("Results_Simulation_RVM_KMLMM_term_project_2016_12_08.csv",sep=";")
+d.rvm<-read.csv("RVM_results_table_KMLMM_term_project_2016_12_17.csv",sep=";")
 str(d.rvm)
 
 setwd(codeDir)
 source("KMLMM_term_project_GERSTENLAUER_utility_functions.R")
+
+#get the table analysing the results, in here we use a pearson covariance
+row.attributes<-c("d$c_setting","d$epsilon_setting","d$polynomial.degree-d$polynomial_degree_setting","d$sparsity","d$cv.mean","d$compu.time")
+column.attributes<-c("d$signal.to.noise.ratio","d$num.observations/d$num.vars","d$polynomial.degree")
+result.table.svm <- populate.table(row.attributes,column.attributes,"spearman")
+colnames(result.table.svm) <- c("Signal to noise ratio", "Number of observations/Number of variables", "Polynomial degree")
+rownames(result.table.svm) <- c("C","Epsilon","Error in estimating poly","Sparsity","CV Mean","Computational Time")
+#save to a new file
+
+Date<-gsub(pattern="-", replacement="_",Sys.Date())
+fileName<-paste("SVM_results_table_KMLMM_term_project_",Date,".csv",sep="")
+result.table.svm
+setwd(dataDir)
+write.table(result.table.svm, file=fileName, append=FALSE, row.names = TRUE, col.names = NA, sep = ";")
