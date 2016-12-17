f37c06cd981ddd6f56c129bbc9ab720a116cca61
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2016-12-17 15:05:25 +0100

    remove lines 25-27 because columns are not swapped any more

--- AnalyseFirstResultsOfSVM.R
+++ AnalyseFirstResultsOfSVM.R
@@ -22,9 +22,6 @@ d  <- read.csv(data.file,sep=";")
 setwd(codeDir)
 source("KMLMM_term_project_GERSTENLAUER_utility_functions.R")
 str(d)
-#there was a mistake in the first run: num.observations and num.vars were mixed up! why?
-names(d)[3]<-"num.observations"
-names(d)[4]<-"num.vars"
 
 with(subset(d,cv.mean>-0.1),plot(signal.to.noise.ratio, cv.mean)) #P.P. the plot is already here?
 #not very informative, (P.P. everyone) nearly always 1!
