1626b0eadc09cd69a3388104497d3c5a60e2dbd7
Francisco Perez <pacogppl@hotmail.com>
2016-12-17 17:20:43 +0100

    Result table output in latex format

--- AnalyseResultsSVMvsRVM.R
+++ AnalyseResultsSVMvsRVM.R
@@ -6,7 +6,7 @@ glue<-function(...){paste(...,sep="")}
 
 #TODO Adapt to working dir or remove!
 #setwd("D:/Documents/MIRI/Semestre 2/APRENDIZAJE AUTOMATICO BASADO EN KERNEL Y MODELADO MULTIVARIANTE/ProyectoFinal/KernelFinalProject/code")
-#setwd("E:/Documents/Mis Documentos/MIRI/Semestre 2/APRENDIZAJE AUTOMATICO BASADO EN KERNEL Y MODELADO MULTIVARIANTE/ProyectoFinal/KernelFinalProject/code")
+setwd("E:/Documents/Mis Documentos/MIRI/Semestre 2/APRENDIZAJE AUTOMATICO BASADO EN KERNEL Y MODELADO MULTIVARIANTE/ProyectoFinal/KernelFinalProject/code")
 #setwd("J:/UPC/2016/02/KMLMM/KernelMethods/practicals/term_project/code")
 
 #define path of standard directories
@@ -35,16 +35,6 @@ str(d.rvm)
 setwd(codeDir)
 source("KMLMM_term_project_GERSTENLAUER_utility_functions.R")
 
-#get the table analysing the results, in here we use a pearson covariance
-row.attributes<-c("d$c_setting","d$epsilon_setting","d$polynomial.degree-d$polynomial_degree_setting","d$sparsity","d$cv.mean","d$compu.time")
-column.attributes<-c("Signal.to.noise.ratio","d$num.observations/d$num.vars","d$polynomial.degree")
-result.table.svm <- populate.table(row.attributes,column.attributes,"spearman")
-colnames(result.table.svm) <- c("Signal to noise ratio", "Number of observations/Number of variables", "Polynomial degree")
-rownames(result.table.svm) <- c("C","Epsilon","Error in estimating poly","Sparsity","CV Mean","Computational Time")
-#save to a new file
+#Obtain the plots for each combination of SVM and RVM 
+#Error in estimated poly
 
-Date<-gsub(pattern="-", replacement="_",Sys.Date())
-fileName<-paste("SVM_results_table_KMLMM_term_project_",Date,".csv",sep="")
-result.table.svm
-setwd(dataDir)
-write.table(result.table.svm, file=fileName, append=FALSE, row.names = TRUE, col.names = NA, sep = ";")
