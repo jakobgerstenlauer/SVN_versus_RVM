eeda09e0802da71e68b27d9fa7ec91e2e824df7e
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2016-12-18 11:58:05 +0100

    average results over 3 replicates, calculate mean and standard deviation, correct cv mean before averaging

--- AnalyseFirstResultsOfRVM.R
+++ AnalyseFirstResultsOfRVM.R
@@ -21,52 +21,70 @@ d<-read.csv("Results_Simulation_RVM_KMLMM_term_project_2016_12_15.csv",sep=";") 
 
 setwd(codeDir)
 source("KMLMM_term_project_GERSTENLAUER_utility_functions.R")
-str(d)
-
-with(d,plot(signal.to.noise.ratio, cv.mean, pch="+")) 
-#There are some extreme outliers with very low cv.mean (very bad models)
-#Let´s ignore these very bad models to be able to plot the relationship between cv.mean and signal to noise ratio.
-
-with(subset(d,cv.mean>-0.1),plot(signal.to.noise.ratio, cv.mean, pch="+")) 
-with(subset(d,cv.mean>-0.1),cor.test(signal.to.noise.ratio, cv.mean)) 
-# Pearson's product-moment correlation
-# 
-# data:  signal.to.noise.ratio and cv.mean
-# t = 1.8427, df = 703, p-value = 0.06579
-# alternative hypothesis: true correlation is not equal to 0
-# 95 percent confidence interval:
-# -0.004529434  0.142443383
-# sample estimates:
-# cor 
-# 0.06933319 
-#Conclusion: The positive correlation is marginally not significant.
-
-#test for non-linear effect with Kendall's tau:
-with(subset(d,cv.mean>-0.1),cor.test(signal.to.noise.ratio, cv.mean, method="kendall"))
-#Kendall's rank correlation tau
-# data:  signal.to.noise.ratio and cv.mean
-# z = 1.6964, p-value = 0.08981
-# alternative hypothesis: true tau is not equal to 0
-# sample estimates:
-# tau 
-# 0.04276472 
-
-with(subset(d,cv.mean>-0.1),hist(cv.mean))
-with(subset(d,cv.mean>-0.1),hist(sparsity))
-with(d,hist(polynomial.degree-polynomial_degree_setting))
-with(d,table(polynomial.degree-polynomial_degree_setting))
-# -7  -6  -5  -4  -3  -2  -1   0   1   2   3   4   5   6   7 
-# 2   8   7  39  44  62  58 188  98 171  60  87  43  30   3
 
 #correct value for cv.mean
 d$cv.mean.corrected <- ifelse(d$cv.mean < 0.0,0.0,d$cv.mean)
 
+#First of all let´s aggregate the recorded values for each sample (each sample has three replicates)
+
+#a dummy data set
+d.rvm<-data.frame(as.numeric(unique(d$id_parameter_combination)))
+names(d.rvm)<-"id_parameter_combination"
+
+#variables to average
+variables.numeric<-names(d)[c(1,7:12)]
+
+for(var in variables.numeric){
+  command<-glue("d.rvm$",var,"<-as.numeric(tapply(d$",var,",d$id_parameter_combination,mean))")
+  eval(parse(text=command))
+  
+  command<-glue("d.rvm$",var,".sd<-as.numeric(tapply(d$",var,",d$id_parameter_combination,sd))")
+  eval(parse(text=command))
+}
+
+#variables specific for the sample, for these variables it is sufficient to record one value (always the same by definition!)
+variables.sample<-names(d)[-c(1,6:12)]
+
+for(var in variables.sample){
+  command<-glue("d.rvm$",var,"<-as.numeric(tapply(d$",var,",d$id_parameter_combination,unique))")
+  eval(parse(text=command))
+}
+
+setwd(dataDir)
+write.table(x=d.rvm, file="Results_Simulation_RVM_KMLMM_term_project_2016_12_15_aggregated.csv",row.names = FALSE)
+
+with(d.rvm,plot(signal.to.noise.ratio, cv.mean.corrected, pch="+")) 
+
+with(d.rvm,plot(signal.to.noise.ratio, cv.mean.corrected, pch="+")) 
+with(d.rvm,cor.test(signal.to.noise.ratio, cv.mean.corrected)) 
+# 
+# Pearson's product-moment correlation
+# 
+# data:  signal.to.noise.ratio and cv.mean.corrected
+# t = 1.3339, df = 298, p-value = 0.1833
+# alternative hypothesis: true correlation is not equal to 0
+# 95 percent confidence interval:
+# -0.03651899  0.18863560
+# sample estimates:
+# cor 
+# 0.07704044 
+
+#test for non-linear effect with Kendall's tau:
+with(d.rvm,cor.test(signal.to.noise.ratio, cv.mean.corrected, method="kendall"))
+
+
+with(d.rvm,hist(cv.mean.corrected))
+with(d.rvm,hist(sparsity))
+with(d.rvm,hist(polynomial.degree-polynomial_degree_setting))
+with(d.rvm,table(polynomial.degree-polynomial_degree_setting))
+
+
 #get the table analysing the results, in here we use a pearson covariance
-row.attributes<-c("d$polynomial.degree-d$polynomial_degree_setting","d$sparsity","d$cv.mean.corrected","d$compu.time")
-column.attributes<-c("d$signal.to.noise.ratio","d$num.observations/d$num.vars","d$polynomial.degree")
+row.attributes<-c("d.rvm$polynomial.degree-d.rvm$polynomial_degree_setting","d.rvm$sparsity","d.rvm$cv.mean.corrected","d.rvm$compu.time")
+column.attributes<-c("d.rvm$signal.to.noise.ratio","d.rvm$num.observations/d.rvm$num.vars","d.rvm$polynomial.degree")
 result.table.rvm <- populate.table(row.attributes,column.attributes,"spearman")
-colnames(result.table.rvm) <- c("Signal to noise ratio", "Number of observations/Number of variables", "Polynomial degree")
-rownames(result.table.rvm) <- c("Error in estimating poly","Sparsity","CV Mean","Computational Time")
+colnames(result.table.rvm) <- c("signal/noise","cases/inputs","degree")
+rownames(result.table.rvm) <- c("$delta$ degree","sparsity","$lambda$","time[s]")
 
 #save to a new file
 Date<-gsub(pattern="-", replacement="_",Sys.Date())
