4dc92bf32ec9dd1d6ac00ff4c0e5009f8b90cb38
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2016-12-19 11:54:04 +0100

    additional analyses which can complement correlation results (linear models with several predictors-> effectively partial correlation analysis).

--- AnalyseResultsSVMvsRVM.R
+++ AnalyseResultsSVMvsRVM.R
@@ -46,6 +46,76 @@ d.vertical$method <- c(rep("svm",nrow(d.svm)),rep("rvm",nrow(d.rvm)))
 setwd(codeDir)
 source("KMLMM_term_project_GERSTENLAUER_utility_functions.R")
 
+#################################################################
+#Model the effect of data attributes on model performance:
+#################################################################
+
+#lambda
+m1.lm<-lm(log(cv.mean.corrected+0.0000001)~
+            signal.to.noise.ratio+polynomial.degree+num.observations+num.vars+(num.observations/num.vars)+method,
+          data=d.vertical)
+plot(d.vertical$cv.mean.corrected, m1.lm$residuals)
+summary(m1.lm)
+# Call:
+#   lm(formula = log(cv.mean.corrected + 1e-07) ~ signal.to.noise.ratio + 
+#        polynomial.degree + num.observations + num.observations/num.vars, 
+#      data = d.vertical)
+# 
+# Residuals:
+#   Min      1Q  Median      3Q     Max 
+# -11.563  -1.436   1.017   2.876  16.280 
+# 
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)               -9.536e-01  8.821e-01  -1.081    0.280    
+# signal.to.noise.ratio      1.261e-01  6.308e-01   0.200    0.842    
+# polynomial.degree         -1.530e+00  1.454e-01 -10.521   <2e-16 ***
+#   num.observations           4.202e-02  2.832e-03  14.838   <2e-16 ***
+#   num.observations:num.vars -3.947e-04  2.719e-05 -14.514   <2e-16 ***
+#   ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Residual standard error: 4.355 on 595 degrees of freedom
+# Multiple R-squared:  0.4085,	Adjusted R-squared:  0.4045 
+# F-statistic: 102.7 on 4 and 595 DF,  p-value: < 2.2e-16
+
+#sparsity
+m1.lm<-lm(log(sparsity+0.0000001)~
+            signal.to.noise.ratio+polynomial.degree+num.observations+num.vars+(num.observations/num.vars)+method,
+          data=d.vertical)
+plot(d.vertical$sparsity, m1.lm$residuals)
+plot(d.vertical$sparsity[d.vertical$sparsity>0], m1.lm$residuals[d.vertical$sparsity>0])
+summary(m1.lm)
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)               -2.056e+00  3.948e-01  -5.209 2.63e-07 ***
+#   signal.to.noise.ratio     -7.551e-01  2.823e-01  -2.675  0.00769 ** 
+#   polynomial.degree          1.335e-01  6.507e-02   2.051  0.04067 *  
+#   num.observations           5.620e-03  1.267e-03   4.434 1.10e-05 ***
+#   num.observations:num.vars -2.250e-05  1.217e-05  -1.849  0.06500 .  
+
+#sparsity
+m1.lm<-lm(polynomial.degree-polynomial_degree_setting~
+            signal.to.noise.ratio+polynomial.degree+num.observations+num.vars+(num.observations/num.vars)+method,
+          data=d.vertical)
+plot(d.vertical$polynomial.degree-d.vertical$polynomial_degree_setting, m1.lm$residuals)
+summary(m1.lm)
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)               -1.754e+00  3.323e-01  -5.277 1.84e-07 ***
+#   signal.to.noise.ratio      3.461e-01  2.250e-01   1.539 0.124418    
+# polynomial.degree          3.672e-01  5.179e-02   7.090 3.82e-12 ***
+#   num.observations           7.119e-03  1.097e-03   6.492 1.79e-10 ***
+#   num.vars                  -2.253e-02  5.956e-03  -3.783 0.000171 ***
+#   methodsvm                  3.689e-01  1.266e-01   2.913 0.003718 ** 
+#   num.observations:num.vars  5.973e-05  3.205e-05   1.864 0.062844 .  
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Residual standard error: 1.551 on 593 degrees of freedom
+# Multiple R-squared:  0.2133,	Adjusted R-squared:  0.2053 
+# F-statistic:  26.8 on 6 and 593 DF,  p-value: < 2.2e-16
+
 require(lattice)
 setwd(plotDir)
 jpeg("Error_Estimating_Polynomial_Degree_boxplot.jpeg")
